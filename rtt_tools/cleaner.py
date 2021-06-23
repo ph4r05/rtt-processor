0f80f6cd2d090d94f1bbd27b1019006629f5c4c9
Dusan Klinec <dusan.klinec@gmail.com>
2021-06-23 16:50:37 +0200

    add RTT experiment cleaner script

---
+++ rtt_tools/cleaner.py
@@ -0,0 +1,141 @@
+#! /usr/bin/python3
+# @author: Dusan Klinec (ph4r05)
+# pandas matplotlib numpy networkx graphviz scipy dill configparser coloredlogs mysqlclient requests sarge cryptography paramiko shellescape
+
+from rtt_tools.common.rtt_db_conn import *
+import configparser
+import re
+import math
+import logging
+import coloredlogs
+import itertools
+import collections
+import json
+import argparse
+from typing import Optional, List, Dict, Tuple, Union, Any, Sequence, Iterable, Collection
+
+
+logger = logging.getLogger(__name__)
+coloredlogs.CHROOT_FILES = []
+coloredlogs.install(level=logging.DEBUG, use_chroot=False)
+EMPTY_SHA = b'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
+EMPTY_SHA_STR = EMPTY_SHA.decode('utf8')
+
+
+def try_execute(fnc, attempts=40, msg=""):
+    for att in range(attempts):
+        try:
+            return fnc()
+
+        except Exception as e:
+            logger.error("Exception in executing function, %s, att=%s, msg=%s" % (e, att, msg))
+            if att - 1 == attempts:
+                raise
+    raise ValueError("Should not happen, failstop")
+
+
+class Cleaner:
+    def __init__(self):
+        self.args = None
+        self.conn = None
+        self.exp_id_low = None
+
+    def proc_args(self, args=None):
+        parser = argparse.ArgumentParser(description='RTT result cleaner')
+        parser.add_argument('--small', dest='small', action='store_const', const=True, default=False,
+                            help='Small result set (few experiments)')
+        self.args, unparsed = parser.parse_known_args()
+        logger.debug("Unparsed: %s" % unparsed)
+
+        # args override
+        if not args:
+            return
+
+        for k in args:
+            setattr(self.args, k, args[k])
+
+    def connect(self):
+        cfg = configparser.ConfigParser()
+        cfg.read("config.ini")
+        self.conn = create_mysql_db_conn(cfg)
+
+    def load_data(self):
+        with self.conn.cursor() as c:
+            logger.info("Processing experiments")
+
+            c.execute("""SELECT id, name, status, created, run_started, run_finished, data_file_sha256, data_file_size, 
+                         data_file_id FROM experiments 
+                         WHERE id >= %s
+                      """ % (self.exp_id_low, ))
+
+            susp_data = []
+            for result in c.fetchall():
+                eid, name, status, sha, fsize = result[0], result[1], result[2], result[6], result[7]
+                data_file_id = result[8]
+
+                if status == 'pending':
+                    continue
+                if sha is None and fsize is None:
+                    continue
+                if (fsize is not None and fsize >= 1024) or (sha is not None and sha != EMPTY_SHA_STR):
+                    continue
+                if data_file_id is None:
+                    logger.info('Suspicious ecp id %s, but empty file id %s' % (eid, result,))
+                    continue
+
+                logger.info('Suspicious experiment result: %s, fsize: %s, sha: %s'
+                            % (eid, fsize, sha))
+
+                # Delete batteries data
+                c.execute('SELECT id FROM batteries WHERE experiment_id=%s', (eid,))
+                bat_ids = [x[0] for x in c.fetchall()]
+                logger.info('battery: %s' % bat_ids)
+
+                for ix, bit in enumerate(bat_ids):
+                    logger.info('Deleting battery records %s for expid %s' % (bit, eid))
+                    try_execute(lambda: c.execute('DELETE FROM batteries WHERE id=%s', (bit,)),
+                                msg="Delete batteries records for expid %s, bID: %s" % (eid, bit))
+
+                # Update jobs
+                logger.info('Update jobs')
+                sql_jobs = 'UPDATE jobs SET status="pending", run_finished=NULL, run_heartbeat=NULL, retries=0 ' \
+                           'WHERE experiment_id=%s'
+                try_execute(lambda: c.execute(sql_jobs, (eid,)),
+                            msg="Update jobs for expid %s" % eid)
+
+                # Update experiment if finished
+                logger.info('Update experiments')
+                nstatus = 'running' if status == 'running' else 'pending'
+                nhash = None if data_file_id is not None else sha
+                nfsize = None if data_file_id is not None else fsize
+                sql_exps = 'UPDATE experiments SET status=%s, run_started=NULL, run_finished=NULL, ' \
+                           'data_file_sha256=%s, data_file_size=%s ' \
+                           'WHERE id=%s'
+                try_execute(lambda: c.execute(sql_exps, (nstatus, nhash, nfsize, eid,)),
+                            msg="Update experiment with ID %s" % eid)
+
+                self.conn.commit()
+                logger.info('Experiment %s solved' % (eid,))
+
+    def init(self, args=None):
+        self.proc_args(args)
+        self.connect()
+
+    def load(self, args=None):
+        self.init(args)
+
+        tstart = time.time()
+        self.load_data()
+        logger.info('Time finished: %s' % (time.time() - tstart))
+
+    def main(self, args=None):
+        self.load(args)
+
+
+def main():
+    l = Cleaner()
+    l.main()
+
+
+if __name__ == "__main__":
+    main()
