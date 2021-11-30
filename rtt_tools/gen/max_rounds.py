4b5f99ffb0f2f9b35b6ce2997a935f4b7c9cc228
Dusan Klinec <dusan.klinec@gmail.com>
2021-11-30 18:12:34 +0100

    rounds: add func info to FuncDb, blocksize, keysize, ivsize

--- rtt_tools/gen/max_rounds.py
+++ rtt_tools/gen/max_rounds.py
@@ -1,3 +1,6 @@
+from typing import Optional
+
+
 class FuncInfo:
     HASH = 1
     CIPHER = 2
@@ -9,13 +12,23 @@ class FuncInfo:
     LIGHT = 3
     MPC = 4
 
-    def __init__(self, fname, ftype, stype=None, max_rounds=None, human_broken_rounds=None, partial_rounds=None):
+    def __init__(self, fname, ftype, stype=None, max_rounds=None, human_broken_rounds=None, partial_rounds=None,
+                 block_size=None, key_size=None, iv_size=None):
         self.fname = fname
         self.ftype = ftype
         self.stype = stype or 0
         self.max_rounds = max_rounds
         self.partial_rounds = partial_rounds
         self.human_broken_rounds = human_broken_rounds
+        self.block_size = block_size
+        self.key_size = key_size
+        self.iv_size = iv_size
+
+    def get_alg_type(self):
+        return get_alg_type(self.ftype, self.stype)
+
+    def __repr__(self):
+        return f'FuncInfo({self.fname}, {self.ftype}, {self.stype}, {self.max_rounds})'
 
 
 class FuncDb:
@@ -45,13 +58,26 @@ class FuncDb:
         for x in collection:
             self.add(x)
 
-    def search(self, fname, ftype=None, return_none=True):
+    def search(self, fname, ftype=None, return_none=True) -> Optional[FuncInfo]:
         k = self._key(fname, ftype)
         if k not in self.recs:
             if return_none:
                 return None
             raise ValueError('Function not present %s' % k)
         return self.recs[k]
+
+
+def get_alg_type(ftype, stype):
+    if ftype == FuncInfo.HASH:
+        return 'hash'
+    elif ftype == FuncInfo.PRNG:
+        return 'prng'
+    elif stype == FuncInfo.STREAM:
+        return 'stream_cipher'
+    elif ftype == FuncInfo.CIPHER and stype in [FuncInfo.BLOCK, FuncInfo.NONE, None]:
+        return 'block'
+    else:
+        raise ValueError(f'Unknown algtype for {ftype}:{stype}')
 
 
 FUNC_DB = FuncDb()
@@ -61,106 +87,106 @@ FUNC_DB.add_all([
     FuncInfo('DICING', FuncInfo.HASH, None, None, None),
     FuncInfo('Dragon', FuncInfo.HASH, None, None, None),
 
-    FuncInfo('AES', FuncInfo.CIPHER, None, 10, 6),
-    FuncInfo('ARIA', FuncInfo.CIPHER, None, 12, 4),
-    FuncInfo('Blowfish', FuncInfo.CIPHER, None, 16, 4),
-    FuncInfo('Camelia', FuncInfo.CIPHER, None, 18, 8),
-    FuncInfo('Camellia', FuncInfo.CIPHER, None, 18, 8),
-    FuncInfo('Cast', FuncInfo.CIPHER, None, 12, 9),
-    FuncInfo('Chaskey', FuncInfo.CIPHER, None, 16, 7),
-    FuncInfo('Fantomas', FuncInfo.CIPHER, None, 12, 4),
-    FuncInfo('Gost', FuncInfo.CIPHER, None, 32, None),
-    FuncInfo('Hight', FuncInfo.CIPHER, None, 32, None),
-    FuncInfo('Idea', FuncInfo.CIPHER, None, 8, 4),
-    FuncInfo('Kasumi', FuncInfo.CIPHER, None, 8, 8),
-    FuncInfo('KUZNYECHIK', FuncInfo.CIPHER, None, 10, 4),
-    FuncInfo('LBLOCK', FuncInfo.CIPHER, None, 32, 24),
-    FuncInfo('LEA', FuncInfo.CIPHER, None, 24, None),
-    FuncInfo('LED', FuncInfo.CIPHER, None, 48, None),
-    FuncInfo('MARS', FuncInfo.CIPHER, None, 16, None),
-    FuncInfo('MISTY1', FuncInfo.CIPHER, None, 8, 6),
-    FuncInfo('NOEKEON', FuncInfo.CIPHER, None, 16, 4),
-    FuncInfo('PICCOLO', FuncInfo.CIPHER, None, 25, None),
-    FuncInfo('PRIDE', FuncInfo.CIPHER, None, 20, 19),
-    FuncInfo('Prince', FuncInfo.CIPHER, None, 12, 6),
-    FuncInfo('RC5-20', FuncInfo.CIPHER, None, 20, None),
-    FuncInfo('RC6', FuncInfo.CIPHER, None, 20, None),
-    FuncInfo('RECTANGLE-K128', FuncInfo.CIPHER, None, 25, 14),
-    FuncInfo('RECTANGLE-K80', FuncInfo.CIPHER, None, 25, 18),
-    FuncInfo('ROAD-RUNNER-K128', FuncInfo.CIPHER, None, 12, None),
-    FuncInfo('ROAD-RUNNER-K80', FuncInfo.CIPHER, None, 10, None),
-    FuncInfo('ROBIN', FuncInfo.CIPHER, None, 16, 16),
-    FuncInfo('ROBIN-STAR', FuncInfo.CIPHER, None, 16, None),
-    FuncInfo('SEED', FuncInfo.CIPHER, None, 16, None),
-    FuncInfo('SERPENT', FuncInfo.CIPHER, None, 32, 5),
-    FuncInfo('SHACAL2', FuncInfo.CIPHER, None, 80, None),
-    FuncInfo('SIMON', FuncInfo.CIPHER, None, 68, 17),
-    FuncInfo('SINGLE-DES', FuncInfo.CIPHER, None, 16, 16),
-    FuncInfo('SPARX-B128', FuncInfo.CIPHER, None, 32, 8),
-    FuncInfo('SPARX-B64', FuncInfo.CIPHER, None, 24, 8),
-    FuncInfo('SPECK', FuncInfo.CIPHER, None, 32, 15),
-    FuncInfo('TEA', FuncInfo.CIPHER, None, 32, 5),
-    FuncInfo('TRIPLE-DES', FuncInfo.CIPHER, None, 16, None),
-    FuncInfo('TWINE', FuncInfo.CIPHER, None, 35, 23),
-    FuncInfo('TWOFISH', FuncInfo.CIPHER, None, 16, 16),
-    FuncInfo('XTEA', FuncInfo.CIPHER, None, 32, 8),
+    FuncInfo('AES', FuncInfo.CIPHER, None, 10, 6, block_size=16, key_size=16, iv_size=16),
+    FuncInfo('ARIA', FuncInfo.CIPHER, None, 12, 4, block_size=16, key_size=16, iv_size=16),
+    FuncInfo('Blowfish', FuncInfo.CIPHER, None, 16, 4, block_size=8, key_size=32, iv_size=16),
+    FuncInfo('Camelia', FuncInfo.CIPHER, None, 18, 8, block_size=16, key_size=16, iv_size=16),
+    FuncInfo('Camellia', FuncInfo.CIPHER, None, 18, 8, block_size=16, key_size=16, iv_size=16),
+    FuncInfo('Cast', FuncInfo.CIPHER, None, 12, 9, block_size=8, key_size=16, iv_size=16),
+    FuncInfo('Chaskey', FuncInfo.CIPHER, None, 16, 7, block_size=16, key_size=16, iv_size=16),
+    FuncInfo('Fantomas', FuncInfo.CIPHER, None, 12, 4, block_size=16, key_size=16, iv_size=16),
+    FuncInfo('Gost', FuncInfo.CIPHER, None, 32, None, block_size=8, key_size=32, iv_size=16),
+    FuncInfo('Hight', FuncInfo.CIPHER, None, 32, None, block_size=8, key_size=16, iv_size=16),
+    FuncInfo('Idea', FuncInfo.CIPHER, None, 8, 4, block_size=8, key_size=16, iv_size=16),
+    FuncInfo('Kasumi', FuncInfo.CIPHER, None, 8, 8, block_size=8, key_size=16, iv_size=16),
+    FuncInfo('KUZNYECHIK', FuncInfo.CIPHER, None, 10, 4, block_size=16, key_size=32, iv_size=16),
+    FuncInfo('LBLOCK', FuncInfo.CIPHER, None, 32, 24, block_size=8, key_size=10, iv_size=16),
+    FuncInfo('LEA', FuncInfo.CIPHER, None, 24, None, block_size=16, key_size=16, iv_size=16),
+    FuncInfo('LED', FuncInfo.CIPHER, None, 48, None, block_size=8, key_size=10, iv_size=16),
+    FuncInfo('MARS', FuncInfo.CIPHER, None, 16, None, block_size=16, key_size=16, iv_size=16),
+    FuncInfo('MISTY1', FuncInfo.CIPHER, None, 8, 6, block_size=8, key_size=16, iv_size=16),
+    FuncInfo('NOEKEON', FuncInfo.CIPHER, None, 16, 4, block_size=16, key_size=16, iv_size=16),
+    FuncInfo('PICCOLO', FuncInfo.CIPHER, None, 25, None, block_size=8, key_size=10, iv_size=16),
+    FuncInfo('PRIDE', FuncInfo.CIPHER, None, 20, 19, block_size=8, key_size=16, iv_size=16),
+    FuncInfo('Prince', FuncInfo.CIPHER, None, 12, 6, block_size=8, key_size=16, iv_size=16),
+    FuncInfo('RC5-20', FuncInfo.CIPHER, None, 20, None, block_size=8, key_size=16, iv_size=16),
+    FuncInfo('RC6', FuncInfo.CIPHER, None, 20, None, block_size=16, key_size=16, iv_size=16),
+    FuncInfo('RECTANGLE-K128', FuncInfo.CIPHER, None, 25, 14, block_size=8, key_size=16, iv_size=16),
+    FuncInfo('RECTANGLE-K80', FuncInfo.CIPHER, None, 25, 18, block_size=8, key_size=10, iv_size=16),
+    FuncInfo('ROAD-RUNNER-K128', FuncInfo.CIPHER, None, 12, None, block_size=8, key_size=16, iv_size=16),
+    FuncInfo('ROAD-RUNNER-K80', FuncInfo.CIPHER, None, 10, None, block_size=8, key_size=10, iv_size=16),
+    FuncInfo('ROBIN', FuncInfo.CIPHER, None, 16, 16, block_size=16, key_size=16, iv_size=16),
+    FuncInfo('ROBIN-STAR', FuncInfo.CIPHER, None, 16, None, block_size=16, key_size=16, iv_size=16),
+    FuncInfo('SEED', FuncInfo.CIPHER, None, 16, None, block_size=16, key_size=16, iv_size=16),
+    FuncInfo('SERPENT', FuncInfo.CIPHER, None, 32, 5, block_size=16, key_size=16, iv_size=16),
+    FuncInfo('SHACAL2', FuncInfo.CIPHER, None, 80, None, block_size=32, key_size=64, iv_size=16),
+    FuncInfo('SIMON', FuncInfo.CIPHER, None, 68, 17, block_size=16, key_size=16, iv_size=16),
+    FuncInfo('SINGLE-DES', FuncInfo.CIPHER, None, 16, 16, block_size=8, key_size=7, iv_size=16),
+    FuncInfo('SPARX-B128', FuncInfo.CIPHER, None, 32, 8, block_size=16, key_size=16, iv_size=16),
+    FuncInfo('SPARX-B64', FuncInfo.CIPHER, None, 24, 8, block_size=8, key_size=16, iv_size=16),
+    FuncInfo('SPECK', FuncInfo.CIPHER, None, 32, 15, block_size=16, key_size=16, iv_size=16),
+    FuncInfo('TEA', FuncInfo.CIPHER, None, 32, 5, block_size=8, key_size=16, iv_size=16),
+    FuncInfo('TRIPLE-DES', FuncInfo.CIPHER, None, 16, None, block_size=8, key_size=21, iv_size=16),
+    FuncInfo('TWINE', FuncInfo.CIPHER, None, 35, 23, block_size=8, key_size=10, iv_size=16),
+    FuncInfo('TWOFISH', FuncInfo.CIPHER, None, 16, 16, block_size=16, key_size=16, iv_size=16),
+    FuncInfo('XTEA', FuncInfo.CIPHER, None, 32, 8, block_size=8, key_size=16, iv_size=16),
 
-    FuncInfo('ARIRANG', FuncInfo.HASH, None, 4, 4),
-    FuncInfo('AURORA', FuncInfo.HASH, None, 17, None),
-    FuncInfo('Abacus', FuncInfo.HASH, None, 280, None),
-    FuncInfo('BLAKE', FuncInfo.HASH, None, 14, 4),
-    FuncInfo('BMW', FuncInfo.HASH, None, 16, None),
-    FuncInfo('Blender', FuncInfo.HASH, None, 32, None),
-    FuncInfo('Boole', FuncInfo.HASH, None, 16, 16),
-    FuncInfo('CHI', FuncInfo.HASH, None, 20, None),
-    FuncInfo('Cheetah', FuncInfo.HASH, None, 16, 12),
-    FuncInfo('CubeHash', FuncInfo.HASH, None, 8, None),
-    FuncInfo('DCH', FuncInfo.HASH, None, 4, 4),
-    FuncInfo('DynamicSHA', FuncInfo.HASH, None, 16, None),
-    FuncInfo('DynamicSHA2', FuncInfo.HASH, None, 17, 17),
-    FuncInfo('ECHO', FuncInfo.HASH, None, 8, 4),
-    FuncInfo('ESSENCE', FuncInfo.HASH, None, 32, None),
-    FuncInfo('Gost', FuncInfo.HASH, None, 10, 5),
-    FuncInfo('Grostl', FuncInfo.HASH, None, 10, None),
-    FuncInfo('Hamsi', FuncInfo.HASH, None, 3, None),
-    FuncInfo('JH', FuncInfo.HASH, None, 42, 10),
-    FuncInfo('Keccak', FuncInfo.HASH, None, 24, 5),
-    FuncInfo('Lesamnta', FuncInfo.HASH, None, 32, 32),
-    FuncInfo('Luffa', FuncInfo.HASH, None, 8, 8),
-    FuncInfo('MCSSHA3', FuncInfo.HASH, None, 1, None),
-    FuncInfo('MD5', FuncInfo.HASH, None, 64, 64),
-    FuncInfo('MD6', FuncInfo.HASH, None, 104, 16),
-    FuncInfo('RIPEMD160', FuncInfo.HASH, None, 80, 48),
-    FuncInfo('SHA1', FuncInfo.HASH, None, 80, 80),
-    FuncInfo('SHA2', FuncInfo.HASH, None, 64, 31),
-    FuncInfo('SHA3', FuncInfo.HASH, None, 24, 5),
-    FuncInfo('SHAvite3', FuncInfo.HASH, None, 12, None),
-    FuncInfo('SIMD', FuncInfo.HASH, None, 4, None),
-    FuncInfo('Sarmal', FuncInfo.HASH, None, 16, None),
-    FuncInfo('Shabal', FuncInfo.HASH, None, 1, None),
-    FuncInfo('Skein', FuncInfo.HASH, None, 72, 17),
-    FuncInfo('TIB3', FuncInfo.HASH, None, 16, None),
-    FuncInfo('Tangle', FuncInfo.HASH, None, 80, 80),
-    FuncInfo('Tangle2', FuncInfo.HASH, None, 80, None),
-    FuncInfo('Tiger', FuncInfo.HASH, None, 23, 19),
-    FuncInfo('Twister', FuncInfo.HASH, None, 9, 9),
-    FuncInfo('Whirlpool', FuncInfo.HASH, None, 10, 6),
+    FuncInfo('ARIRANG', FuncInfo.HASH, None, 4, 4, block_size=32),
+    FuncInfo('AURORA', FuncInfo.HASH, None, 17, None, block_size=32),
+    FuncInfo('Abacus', FuncInfo.HASH, None, 280, None, block_size=32),
+    FuncInfo('BLAKE', FuncInfo.HASH, None, 14, 4, block_size=32),
+    FuncInfo('BMW', FuncInfo.HASH, None, 16, None, block_size=32),
+    FuncInfo('Blender', FuncInfo.HASH, None, 32, None, block_size=64),
+    FuncInfo('Boole', FuncInfo.HASH, None, 16, 16, block_size=32),
+    FuncInfo('CHI', FuncInfo.HASH, None, 20, None, block_size=64),
+    FuncInfo('Cheetah', FuncInfo.HASH, None, 16, 12, block_size=32),
+    FuncInfo('CubeHash', FuncInfo.HASH, None, 8, None, block_size=32),
+    FuncInfo('DCH', FuncInfo.HASH, None, 4, 4, block_size=32),
+    FuncInfo('DynamicSHA', FuncInfo.HASH, None, 16, None, block_size=48),
+    FuncInfo('DynamicSHA2', FuncInfo.HASH, None, 17, 17, block_size=32),
+    FuncInfo('ECHO', FuncInfo.HASH, None, 8, 4, block_size=32),
+    FuncInfo('ESSENCE', FuncInfo.HASH, None, 32, None, block_size=32),
+    FuncInfo('Gost', FuncInfo.HASH, None, 10, 5, block_size=32),
+    FuncInfo('Grostl', FuncInfo.HASH, None, 10, None, block_size=32),
+    FuncInfo('Hamsi', FuncInfo.HASH, None, 3, None, block_size=32),
+    FuncInfo('JH', FuncInfo.HASH, None, 42, 10, block_size=32),
+    FuncInfo('Keccak', FuncInfo.HASH, None, 24, 5, block_size=32),
+    FuncInfo('Lesamnta', FuncInfo.HASH, None, 32, 32, block_size=32),
+    FuncInfo('Luffa', FuncInfo.HASH, None, 8, 8, block_size=32),
+    FuncInfo('MCSSHA3', FuncInfo.HASH, None, 1, None, block_size=32),
+    FuncInfo('MD5', FuncInfo.HASH, None, 64, 64, block_size=16),
+    FuncInfo('MD6', FuncInfo.HASH, None, 104, 16, block_size=32),
+    FuncInfo('RIPEMD160', FuncInfo.HASH, None, 80, 48, block_size=20),
+    FuncInfo('SHA1', FuncInfo.HASH, None, 80, 80, block_size=20),
+    FuncInfo('SHA2', FuncInfo.HASH, None, 64, 31, block_size=32),
+    FuncInfo('SHA3', FuncInfo.HASH, None, 24, 5, block_size=32),
+    FuncInfo('SHAvite3', FuncInfo.HASH, None, 12, None, block_size=32),
+    FuncInfo('SIMD', FuncInfo.HASH, None, 4, None, block_size=32),
+    FuncInfo('Sarmal', FuncInfo.HASH, None, 16, None, block_size=32),
+    FuncInfo('Shabal', FuncInfo.HASH, None, 1, None, block_size=32),
+    FuncInfo('Skein', FuncInfo.HASH, None, 72, 17, block_size=32),
+    FuncInfo('TIB3', FuncInfo.HASH, None, 16, None, block_size=48),
+    FuncInfo('Tangle', FuncInfo.HASH, None, 80, 80, block_size=32),
+    FuncInfo('Tangle2', FuncInfo.HASH, None, 80, None, block_size=32),
+    FuncInfo('Tiger', FuncInfo.HASH, None, 23, 19, block_size=24),
+    FuncInfo('Twister', FuncInfo.HASH, None, 9, 9, block_size=32),
+    FuncInfo('Whirlpool', FuncInfo.HASH, None, 10, 6, block_size=64),
 
-    FuncInfo('Chacha', FuncInfo.CIPHER, FuncInfo.STREAM, 20, None),
-    FuncInfo('DECIM', FuncInfo.CIPHER, FuncInfo.STREAM, 8, None),
-    FuncInfo('F-FCSR', FuncInfo.CIPHER, FuncInfo.STREAM, 5, None),
-    FuncInfo('Fubuki', FuncInfo.CIPHER, FuncInfo.STREAM, 4, None),
-    FuncInfo('Grain', FuncInfo.CIPHER, FuncInfo.STREAM, 13, None),
-    FuncInfo('HC-128', FuncInfo.CIPHER, FuncInfo.STREAM, 1, None),
-    FuncInfo('Hermes', FuncInfo.CIPHER, FuncInfo.STREAM, 10, None),
-    FuncInfo('LEX', FuncInfo.CIPHER, FuncInfo.STREAM, 10, None),
-    FuncInfo('MICKEY', FuncInfo.CIPHER, FuncInfo.STREAM, 1, None),
-    FuncInfo('RC4', FuncInfo.CIPHER, FuncInfo.STREAM, 1, None),
-    FuncInfo('Rabbit', FuncInfo.CIPHER, FuncInfo.STREAM, 4, None),
-    FuncInfo('SOSEMANUK', FuncInfo.CIPHER, FuncInfo.STREAM, 25, None),
-    FuncInfo('Salsa20', FuncInfo.CIPHER, FuncInfo.STREAM, 20, None),
-    FuncInfo('TSC-4', FuncInfo.CIPHER, FuncInfo.STREAM, 32, None),
-    FuncInfo('Trivium', FuncInfo.CIPHER, FuncInfo.STREAM, 9, None),
+    FuncInfo('Chacha', FuncInfo.CIPHER, FuncInfo.STREAM, 20, None, block_size=32, key_size=32, iv_size=8),
+    FuncInfo('DECIM', FuncInfo.CIPHER, FuncInfo.STREAM, 8, None, block_size=24, key_size=10, iv_size=8),
+    FuncInfo('F-FCSR', FuncInfo.CIPHER, FuncInfo.STREAM, 5, None, block_size=16, key_size=16, iv_size=8),
+    FuncInfo('Fubuki', FuncInfo.CIPHER, FuncInfo.STREAM, 4, None, block_size=16, key_size=16, iv_size=16),
+    FuncInfo('Grain', FuncInfo.CIPHER, FuncInfo.STREAM, 13, None, block_size=16, key_size=16, iv_size=12),
+    FuncInfo('HC-128', FuncInfo.CIPHER, FuncInfo.STREAM, 1, None, block_size=16, key_size=16, iv_size=16),
+    FuncInfo('Hermes', FuncInfo.CIPHER, FuncInfo.STREAM, 10, None, block_size=16, key_size=10, iv_size=0),
+    FuncInfo('LEX', FuncInfo.CIPHER, FuncInfo.STREAM, 10, None, block_size=16, key_size=16, iv_size=16),
+    FuncInfo('MICKEY', FuncInfo.CIPHER, FuncInfo.STREAM, 1, None, block_size=16, key_size=16, iv_size=0),
+    FuncInfo('RC4', FuncInfo.CIPHER, FuncInfo.STREAM, 1, None, block_size=32, key_size=16, iv_size=0),
+    FuncInfo('Rabbit', FuncInfo.CIPHER, FuncInfo.STREAM, 4, None, block_size=16, key_size=16, iv_size=8),
+    FuncInfo('SOSEMANUK', FuncInfo.CIPHER, FuncInfo.STREAM, 25, None, block_size=16, key_size=16, iv_size=16),
+    FuncInfo('Salsa20', FuncInfo.CIPHER, FuncInfo.STREAM, 20, None, block_size=8, key_size=16, iv_size=8),
+    FuncInfo('TSC-4', FuncInfo.CIPHER, FuncInfo.STREAM, 32, None, block_size=32, key_size=10, iv_size=10),
+    FuncInfo('Trivium', FuncInfo.CIPHER, FuncInfo.STREAM, 9, None, block_size=8, key_size=10, iv_size=10),
 
     FuncInfo('gmimc-S45a', FuncInfo.CIPHER, FuncInfo.MPC, 121),
     FuncInfo('gmimc-S45b', FuncInfo.CIPHER, FuncInfo.MPC, 137),
